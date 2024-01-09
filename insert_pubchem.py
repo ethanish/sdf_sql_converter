8c6756d320d79b836dff2dcfd1a08198abdcfa2a
ethanish <inseungish@gmail.com>
2024-01-09 09:29:45 +0900

    [ADD] Utils for insert txt file

--- insert_pubchem.py
+++ insert_pubchem.py
@@ -1,7 +1,8 @@
 from zipfile import ZipFile 
 import gzip 
 import os 
- 
+import pandas as pd
+
 class directory_crawler: 
     def __init__(self, init_path: str): 
         self._root_directory = init_path 
@@ -25,6 +26,7 @@ class directory_crawler:
                 yield filepath
 
 class file_loader(directory_crawler):
+    total_sdf = -1
     file_name_list = []
     current_file_name = None
     file_cursor = -1
@@ -50,6 +52,7 @@ class file_loader(directory_crawler):
             # Archive file
             if (self.is_file_format(file_name, self._file_format + ".zip")):
                 self.file_name_list.append(file_name)
+        self.total_sdf = len(self.file_name_list)
 
     def is_file_format(self, file_name: str, file_format: str) -> bool:
         # TODO: Change to uppder case automatically
@@ -97,8 +100,33 @@ class file_loader(directory_crawler):
 
         return contents_buffer
 
+class convert_sdf_sql(file_loader):
+    def __init__(self, init_path, file_format=".sdf"):
+        #Parameter
+        self._root_directory = init_path
+        self._file_format = file_format
+        #Init Function
+        self.filter_file_format()
+        self.get_next_file_cursor()
+        self.current_mol_content = self.get_sdf_item()
 
-# class insert_sdf_db:
+    def create_sdf_sql(self):
+        # create as pandas df 
+        # pandas df to sql
+
+        pass
+
+    def generate_sdf_sql(self):
+        # Create to yield for generate function
+
+        pass
+
+    def save_as(self, type=".sql"):
+        # Type: .sql, .txt, ...
+
+        pass
+
+# class insert_sdf_db:asfd
 #     def __init__(self, directory, target_file_format):
 #         self._root_directory = directory
 #         # initializing empty file paths list 
@@ -117,17 +145,27 @@ class file_loader(directory_crawler):
 #                 file_content = f.read()
 #         print(file_content)
 #         return file_content
-    
+
 def test_code(): 
     sdf_full_path = '/mnt/d/DATABASES/PubChem/Compound/FULL/' 
     # calling function to get all file paths in the directory
-    iidb_obj = file_loader(sdf_full_path, '.sdf')
-    # printing the list of all files to be zipped 
-    iidb_obj.get_next_file_cursor()
-    one_mol_content = iidb_obj.get_sdf_item()
-    # print(iidb_obj.get_all_file_paths())
-    print("MOL 1")
-    print(one_mol_content)
+    iidb_obj = convert_sdf_sql(sdf_full_path, '.sdf')
+    
+    # print(iidb_obj.current_mol_content)
+
+    test_line = iidb_obj.current_mol_content
+
+    splited_mol_file = test_line.split("\n")
+    # Removed index & last end motif $$$$
+    current_contents = "\n".join(splited_mol_file[1:-2])
+    # Split again with appendix information indicator ">"
+    splited_current_contents = current_contents.split(">")
+
+    current_mol_dict = {}
+    current_mol_dict['index'] = splited_mol_file[0]
+    current_mol_dict['mol'] = splited_current_contents[0]
+    
+
 
 
 if __name__ == "__main__": 
---
+++ utils/insert_externals.py
@@ -0,0 +1,60 @@
+import os
+import json
+import pandas as pd
+
+CURRENT_FILE_PATH = os.path.abspath(__file__)
+CURRENT_DIRECTORY = os.path.dirname(CURRENT_FILE_PATH)
+DB_CONFIG_PATH = f"%s/../external_data/chembl_uniprot_mapping.txt"%(CURRENT_DIRECTORY)
+
+def load_db_config(file_path):
+    with open(file_path, 'r') as config_file:
+        db_config = json.load(config_file)
+    return db_config
+
+def is_exists_db_table():
+    print()
+
+def create_db_table(db_existance_flag:bool):
+    print()
+
+def insert_txt_as_sql(file_path, conn):
+    try:
+        connector_conn = mysql.connector.connect(**conn)
+        connector_cursor = mysql_conn.cursor()
+
+        with open(file_path, 'r') as file:
+            insert_lines = file.readlines()
+
+            for insert_line in insert_lines:
+                if insert_line[0] == "#":
+                    continue
+                # TODO: clear here
+                insert_data = insert_line.strip().split(',')
+                insert_query = f"INSERT INTO your_table_name (column1, column2, column3) VALUES ('{insert_data[0]}', '{insert_data[1]}', '{insert_data[2]}');"
+
+                # connector_cursor.execute(insert_query)
+                print(insert_query)
+
+        connector_conn.commit()
+
+        print("[COMPLETED] Successfully Inserted.")
+
+    except Exception as e:
+        print(f"[ERROR] {e}")
+
+    finally:
+        connector_cursor.close()
+        connector_conn.close()
+
+def insert_sql_chembl_uniprot_mapping_txt():
+    db_config = load_db_config(DB_CONFIG_PATH)
+    # insert_table_name = "table_name"
+    # if not is_exists_db_table(insert_table_name):
+    #     create_db_table(True, db_config, insert_table_name)
+    
+    chembl_uniprot_maping_txt_loc = f"%s/%s"%(CURRENT_DIRECTORY, "../external_data/chembl_uniprot_mapping.txt")
+    insert_txt_as_sql(chembl_uniprot_maping_txt_loc, db_config)
+
+if __name__ == "__main__":
+    insert_sql_chembl_uniprot_mapping_txt()
+    
